d7354b518e3b920490e8fb146ab787d4910b1d58
anibal <anibal@git.com>
2018-06-26 09:53:58 -0500

    se agrego la vista individual de las peliculas en movie_info.html, tambien se agrago el campo de sinopsis en la base de datos

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = '/home/anibal/Documentos/projects/moviesoft/static'
 ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
 
 app = Flask(__name__)
-# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # modus = Modus(app)
@@ -22,6 +22,7 @@ class Movie(db.Model):
     category = db.Column(db.String(80), unique=False, nullable=False)
     director = db.Column(db.String(80), unique=False, nullable=False)
     distributed  = db.Column(db.String(80), unique=False, nullable=False)
+    sinopsis = db.Column(db.String(200), unique=False, nullable=False)
     image = db.Column(db.String(80), unique=False)
 
     def __repr__(self):
@@ -39,12 +40,13 @@ def newMovie():
 def viewMovie():
     return render_template('viewMovies.html', movies=Movie.query.all())
 
-@app.route('/moviesInfo/<int:id>', methods=['GET', 'POST'])
-def movieInfo():
+@app.route('/moviesInfo/<int:id>', methods=['GET'])
+def movieInfo(id):
     movie_info = Movie.query.filter_by(id=id).first()
     if movie_info:
-        return render_template('movie_Info.html', movies=Movie.query.all())
+        return render_template('movie_Info.html', movies=movie_info)
     return "pelicula no encontrada"
+
 
 ###############################################Add movie###############################################
 @app.route('/movies/', methods=['GET', 'POST'])
@@ -55,10 +57,11 @@ def movie():
         category = request.form['category']
         director = request.form['director']
         distributed = request.form['distributed']
+        sinopsis  = request.form['sinopsis']
         image = request.files['image']
         image_name = secure_filename(image.filename)
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
-        movie = Movie(name=name, year=year, category=category, director=director, distributed=distributed, image=image_name)
+        movie = Movie(name=name, year=year, category=category, director=director, distributed=distributed, sinopsis=sinopsis, image=image_name)
         db.session.add(movie)
         db.session.commit()
         return redirect(url_for('movie'))
@@ -94,22 +97,23 @@ def update(movie_id):
     movie_update = Movie.query.filter_by(id=movie_id).first()
     if request.method == 'GET':
         return render_template('search.html', movie=movie_update)
-    if request.method == 'POST':
-        movie_update.name = request.form['name']
-        movie_update.year = request.form['year']
-        movie_update.category = request.form['category']
-        movie_update.director = request.form['director']
-        movie_update.distributed = request.form['distributed']
-        print(request.files['image'])
-        if 'image' in request.files:
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], movie_update.image))
-            image = request.files['image']
-            print('image')
-            image_name = secure_filename(image.filename)
-            print('image_name')
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
-        db.session.commit()
+    # if request.method == 'POST':
+    movie_update.name = request.form['name']
+    movie_update.year = request.form['year']
+    movie_update.category = request.form['category']
+    movie_update.director = request.form['director']
+    movie_update.distributed = request.form['distributed']
+    movie_update.sinopsis = request.form['sinopsis']
+    if not 'image' in request.files:
         return render_template('search.html', movie=movie_update)
+    movie_new_image = request.files['image']
+    if 'image' in request.files:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], movie_update.image))
+        image_name = secure_filename(movie_new_image.filename)
+        movie_update.image = image_name
+        movie_new_image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
+    db.session.commit()
+    return render_template('search.html', movie=movie_update)
 
 
 
